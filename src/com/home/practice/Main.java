b28410010f8a20972aa6e16913c61798860b75ca
Udit Anand <udianand@users.noreply.github.com>
2021-12-27 19:05:13 +0530

    CompletableFuture Basic

--- src/com/home/practice/Main.java
+++ src/com/home/practice/Main.java
@@ -1,6 +1,7 @@
 package com.home.practice;
 
 import java.util.HashSet;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,19 @@ class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       ExecutorService executorService =  Executors.newFixedThreadPool(2);
+
+        CompletableFuture completableFuture = CompletableFuture.supplyAsync(()->{
+            System.out.println("Hello from Supply Async");
+            return null;
+        },executorService);
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
